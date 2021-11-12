89597d4b212472c17eab8b76494071c973845f21
Huy Tran <max@gotitapp.co>
2021-11-12 14:54:25 +0700

    feat: Finish intro section

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,5 @@
-import Logo from '../assets/images/devjane.png'
+import Logo from '../assets/images/devjane.png';
+import styles from '../assets/styles/nav.module.css';
 
 export default function Header() {
   return (
@@ -8,7 +9,7 @@ export default function Header() {
         <button className="nav-toggle" aria-label="toggle navigation">
           <span className="hamburger"></span>
         </button>
-        <nav className="nav">
+        <nav className={styles.nav}>
           <ul className="nav__list">
             <li className="nav__item"><a href="#home">Home</a></li>
             <li className="nav__item"><a href="#services">My Services</a></li>
--- src/components/Introduction.tsx
+++ src/components/Introduction.tsx
@@ -1,15 +1,16 @@
-import DevJane from '../assets/images/dev-jane-01.jpg';
+import DevJane from '../assets/images/dev-jane-03.png';
+import styles from '../assets/styles/intro.module.css';
 
 export default function Introduction() {
   return (
-    <section className="intro" id="home">
+    <section className={styles.intro} id="home">
       <h1 className="section__title section__title--intro">
         Hi, I am <strong>Jane Smith</strong>
       </h1>
-      <p className="section__subtitle section__subtitle--intro">
+      <p className={`section__subtitle section__subtitle--intro ${styles.subtitle}`}>
         front-end dev
       </p>
-      <img src={DevJane} alt="Dev jane smiling" />
+      <img className={styles.img} src={DevJane} alt="Dev jane smiling" />
     </section>
   )
 }
